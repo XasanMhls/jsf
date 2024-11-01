aee5ab35a7b7dc7627490f2f87c4e219144c3222
XasanMhls <nasapandau@gmail.com>
2024-11-01 14:11:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let user = prompt ('Ismingizni kiritng');
+alert (user)
+console.log (user);
+let famiya = prompt ('Familyangizni kiriting');
+alert (famiya)
+console.log (famiya);
+let age = prompt ('Yoshingizni kiriting');
+alert (age)
+console.log (age);
+let subject = prompt ('Yoqtirgan fanizi yozing');
+alert (subject)
+console.log (subject);
+let locat = prompt ('Yashaydigan tumaningizni kiriting');
+alert (locat)
+console.log (locat);
+let submit = ('malumotlarni kiritganiz uchun tashakkur');
+alert (submit)
+console.log (submit);
